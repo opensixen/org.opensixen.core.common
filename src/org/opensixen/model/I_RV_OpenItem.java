d5c0f1e1e1b4a66c71d84da29447a9a40fa9202b
Eloy Gomez <eloy@indeos.es>
2011-02-16 20:04:03 +0000

    Añadimos vistas modelos para la vista RV_OpenItem y soporta para usarla desde JasperFormater

---
+++ src/org/opensixen/model/I_RV_OpenItem.java
@@ -0,0 +1,393 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.opensixen.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for RV_OpenItem
+ *  @author Adempiere (generated) 
+ *  @version Release 3.6.0LTS
+ */
+public interface I_RV_OpenItem 
+{
+
+    /** TableName=RV_OpenItem */
+    public static final String Table_Name = "RV_OpenItem";
+
+    /** AD_Table_ID=413 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 3 - Client - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(3);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Client.
+	  * Client/Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within client
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within client
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name C_Activity_ID */
+    public static final String COLUMNNAME_C_Activity_ID = "C_Activity_ID";
+
+	/** Set Activity.
+	  * Business Activity
+	  */
+	public void setC_Activity_ID (int C_Activity_ID);
+
+	/** Get Activity.
+	  * Business Activity
+	  */
+	public int getC_Activity_ID();
+
+	public org.compiere.model.I_C_Activity getC_Activity() throws RuntimeException;
+
+    /** Column name C_BPartner_ID */
+    public static final String COLUMNNAME_C_BPartner_ID = "C_BPartner_ID";
+
+	/** Set Business Partner .
+	  * Identifies a Business Partner
+	  */
+	public void setC_BPartner_ID (int C_BPartner_ID);
+
+	/** Get Business Partner .
+	  * Identifies a Business Partner
+	  */
+	public int getC_BPartner_ID();
+
+	public org.compiere.model.I_C_BPartner getC_BPartner() throws RuntimeException;
+
+    /** Column name C_Campaign_ID */
+    public static final String COLUMNNAME_C_Campaign_ID = "C_Campaign_ID";
+
+	/** Set Campaign.
+	  * Marketing Campaign
+	  */
+	public void setC_Campaign_ID (int C_Campaign_ID);
+
+	/** Get Campaign.
+	  * Marketing Campaign
+	  */
+	public int getC_Campaign_ID();
+
+	public org.compiere.model.I_C_Campaign getC_Campaign() throws RuntimeException;
+
+    /** Column name C_ConversionType_ID */
+    public static final String COLUMNNAME_C_ConversionType_ID = "C_ConversionType_ID";
+
+	/** Set Currency Type.
+	  * Currency Conversion Rate Type
+	  */
+	public void setC_ConversionType_ID (int C_ConversionType_ID);
+
+	/** Get Currency Type.
+	  * Currency Conversion Rate Type
+	  */
+	public int getC_ConversionType_ID();
+
+	public org.compiere.model.I_C_ConversionType getC_ConversionType() throws RuntimeException;
+
+    /** Column name C_Currency_ID */
+    public static final String COLUMNNAME_C_Currency_ID = "C_Currency_ID";
+
+	/** Set Currency.
+	  * The Currency for this record
+	  */
+	public void setC_Currency_ID (int C_Currency_ID);
+
+	/** Get Currency.
+	  * The Currency for this record
+	  */
+	public int getC_Currency_ID();
+
+	public org.compiere.model.I_C_Currency getC_Currency() throws RuntimeException;
+
+    /** Column name C_Invoice_ID */
+    public static final String COLUMNNAME_C_Invoice_ID = "C_Invoice_ID";
+
+	/** Set Invoice.
+	  * Invoice Identifier
+	  */
+	public void setC_Invoice_ID (int C_Invoice_ID);
+
+	/** Get Invoice.
+	  * Invoice Identifier
+	  */
+	public int getC_Invoice_ID();
+
+	public org.compiere.model.I_C_Invoice getC_Invoice() throws RuntimeException;
+
+    /** Column name C_InvoicePaySchedule_ID */
+    public static final String COLUMNNAME_C_InvoicePaySchedule_ID = "C_InvoicePaySchedule_ID";
+
+	/** Set Invoice Payment Schedule.
+	  * Invoice Payment Schedule
+	  */
+	public void setC_InvoicePaySchedule_ID (int C_InvoicePaySchedule_ID);
+
+	/** Get Invoice Payment Schedule.
+	  * Invoice Payment Schedule
+	  */
+	public int getC_InvoicePaySchedule_ID();
+
+	public org.compiere.model.I_C_InvoicePaySchedule getC_InvoicePaySchedule() throws RuntimeException;
+
+    /** Column name C_Order_ID */
+    public static final String COLUMNNAME_C_Order_ID = "C_Order_ID";
+
+	/** Set Order.
+	  * Order
+	  */
+	public void setC_Order_ID (int C_Order_ID);
+
+	/** Get Order.
+	  * Order
+	  */
+	public int getC_Order_ID();
+
+	public org.compiere.model.I_C_Order getC_Order() throws RuntimeException;
+
+    /** Column name C_PaymentTerm_ID */
+    public static final String COLUMNNAME_C_PaymentTerm_ID = "C_PaymentTerm_ID";
+
+	/** Set Payment Term.
+	  * The terms of Payment (timing, discount)
+	  */
+	public void setC_PaymentTerm_ID (int C_PaymentTerm_ID);
+
+	/** Get Payment Term.
+	  * The terms of Payment (timing, discount)
+	  */
+	public int getC_PaymentTerm_ID();
+
+	public org.compiere.model.I_C_PaymentTerm getC_PaymentTerm() throws RuntimeException;
+
+    /** Column name C_Project_ID */
+    public static final String COLUMNNAME_C_Project_ID = "C_Project_ID";
+
+	/** Set Project.
+	  * Financial Project
+	  */
+	public void setC_Project_ID (int C_Project_ID);
+
+	/** Get Project.
+	  * Financial Project
+	  */
+	public int getC_Project_ID();
+
+	public org.compiere.model.I_C_Project getC_Project() throws RuntimeException;
+
+    /** Column name DateAcct */
+    public static final String COLUMNNAME_DateAcct = "DateAcct";
+
+	/** Set Account Date.
+	  * Accounting Date
+	  */
+	public void setDateAcct (Timestamp DateAcct);
+
+	/** Get Account Date.
+	  * Accounting Date
+	  */
+	public Timestamp getDateAcct();
+
+    /** Column name DateInvoiced */
+    public static final String COLUMNNAME_DateInvoiced = "DateInvoiced";
+
+	/** Set Date Invoiced.
+	  * Date printed on Invoice
+	  */
+	public void setDateInvoiced (Timestamp DateInvoiced);
+
+	/** Get Date Invoiced.
+	  * Date printed on Invoice
+	  */
+	public Timestamp getDateInvoiced();
+
+    /** Column name DaysDue */
+    public static final String COLUMNNAME_DaysDue = "DaysDue";
+
+	/** Set Days due.
+	  * Number of days due (negative: due in number of days)
+	  */
+	public void setDaysDue (int DaysDue);
+
+	/** Get Days due.
+	  * Number of days due (negative: due in number of days)
+	  */
+	public int getDaysDue();
+
+    /** Column name DiscountAmt */
+    public static final String COLUMNNAME_DiscountAmt = "DiscountAmt";
+
+	/** Set Discount Amount.
+	  * Calculated amount of discount
+	  */
+	public void setDiscountAmt (BigDecimal DiscountAmt);
+
+	/** Get Discount Amount.
+	  * Calculated amount of discount
+	  */
+	public BigDecimal getDiscountAmt();
+
+    /** Column name DiscountDate */
+    public static final String COLUMNNAME_DiscountDate = "DiscountDate";
+
+	/** Set Discount Date.
+	  * Last Date for payments with discount
+	  */
+	public void setDiscountDate (Timestamp DiscountDate);
+
+	/** Get Discount Date.
+	  * Last Date for payments with discount
+	  */
+	public Timestamp getDiscountDate();
+
+    /** Column name DocumentNo */
+    public static final String COLUMNNAME_DocumentNo = "DocumentNo";
+
+	/** Set Document No.
+	  * Document sequence number of the document
+	  */
+	public void setDocumentNo (String DocumentNo);
+
+	/** Get Document No.
+	  * Document sequence number of the document
+	  */
+	public String getDocumentNo();
+
+    /** Column name DueDate */
+    public static final String COLUMNNAME_DueDate = "DueDate";
+
+	/** Set Due Date.
+	  * Date when the payment is due
+	  */
+	public void setDueDate (Timestamp DueDate);
+
+	/** Get Due Date.
+	  * Date when the payment is due
+	  */
+	public Timestamp getDueDate();
+
+    /** Column name GrandTotal */
+    public static final String COLUMNNAME_GrandTotal = "GrandTotal";
+
+	/** Set Grand Total.
+	  * Total amount of document
+	  */
+	public void setGrandTotal (BigDecimal GrandTotal);
+
+	/** Get Grand Total.
+	  * Total amount of document
+	  */
+	public BigDecimal getGrandTotal();
+
+    /** Column name InvoiceCollectionType */
+    public static final String COLUMNNAME_InvoiceCollectionType = "InvoiceCollectionType";
+
+	/** Set Collection Status.
+	  * Invoice Collection Status
+	  */
+	public void setInvoiceCollectionType (String InvoiceCollectionType);
+
+	/** Get Collection Status.
+	  * Invoice Collection Status
+	  */
+	public String getInvoiceCollectionType();
+
+    /** Column name IsPayScheduleValid */
+    public static final String COLUMNNAME_IsPayScheduleValid = "IsPayScheduleValid";
+
+	/** Set Pay Schedule valid.
+	  * Is the Payment Schedule is valid
+	  */
+	public void setIsPayScheduleValid (boolean IsPayScheduleValid);
+
+	/** Get Pay Schedule valid.
+	  * Is the Payment Schedule is valid
+	  */
+	public boolean isPayScheduleValid();
+
+    /** Column name IsSOTrx */
+    public static final String COLUMNNAME_IsSOTrx = "IsSOTrx";
+
+	/** Set Sales Transaction.
+	  * This is a Sales Transaction
+	  */
+	public void setIsSOTrx (boolean IsSOTrx);
+
+	/** Get Sales Transaction.
+	  * This is a Sales Transaction
+	  */
+	public boolean isSOTrx();
+
+    /** Column name NetDays */
+    public static final String COLUMNNAME_NetDays = "NetDays";
+
+	/** Set Net Days.
+	  * Net Days in which payment is due
+	  */
+	public void setNetDays (int NetDays);
+
+	/** Get Net Days.
+	  * Net Days in which payment is due
+	  */
+	public int getNetDays();
+
+    /** Column name OpenAmt */
+    public static final String COLUMNNAME_OpenAmt = "OpenAmt";
+
+	/** Set Open Amount.
+	  * Open item amount
+	  */
+	public void setOpenAmt (BigDecimal OpenAmt);
+
+	/** Get Open Amount.
+	  * Open item amount
+	  */
+	public BigDecimal getOpenAmt();
+
+    /** Column name PaidAmt */
+    public static final String COLUMNNAME_PaidAmt = "PaidAmt";
+
+	/** Set Paid Amount	  */
+	public void setPaidAmt (BigDecimal PaidAmt);
+
+	/** Get Paid Amount	  */
+	public BigDecimal getPaidAmt();
+}
---
+++ src/org/opensixen/model/MVOpenItem.java
@@ -0,0 +1,30 @@
+package org.opensixen.model;
+
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Properties;
+
+public class MVOpenItem extends X_RV_OpenItem {
+
+	public MVOpenItem(Properties ctx, int RV_OpenItem_ID, String trxName) {
+		super(ctx, RV_OpenItem_ID, trxName);
+	}
+
+	public MVOpenItem(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+
+	/**
+	 * Get OpenItems for this invoice
+	 * @param ctx
+	 * @param C_Invoice_ID
+	 * @return
+	 */
+	public static List<MVOpenItem> getFromInvoice(Properties ctx, int C_Invoice_ID)	{
+		String[] order = { COLUMNNAME_DueDate };
+		QParam[] params = { new QParam(COLUMNNAME_C_Invoice_ID, C_Invoice_ID) };
+		List<MVOpenItem> items = POFactory.getList(ctx, MVOpenItem.class, params,order, null);
+		return items;
+	}
+	
+}
