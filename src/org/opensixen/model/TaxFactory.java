3d1936a6186086bef11ea02d7f089e67c22cfadc
Eloy Gomez <eloy@indeos.es>
2011-06-13 16:26:47 +0000

    Añadimos funcionalidad para gestionar cargos desde TaxFactory

--- src/org/opensixen/model/TaxFactory.java
+++ src/org/opensixen/model/TaxFactory.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import org.adempiere.exceptions.TaxCriteriaNotFoundException;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.MBPartnerLocation;
+import org.compiere.model.MCharge;
 import org.compiere.model.MProduct;
 import org.opensixen.exceptions.TaxCriteriaNotImplemented;
 import org.opensixen.osgi.interfaces.ITaxFactory;
@@ -20,9 +21,6 @@ public class TaxFactory implements ITaxFactory {
 			int shipC_BPartner_Location_ID, boolean IsSOTrx)
 			throws TaxCriteriaNotFoundException {
 
-		if (C_Charge_ID != 0)	{
-			throw new TaxCriteriaNotImplemented("This factory can't manage charges. Delegate to standard Tax factory");
-		}
 		MBPartnerLocation bill_location = POFactory.get(MBPartnerLocation.class, new QParam(MBPartnerLocation.COLUMNNAME_C_BPartner_Location_ID, billC_BPartner_Location_ID));
 		I_C_BPartner bp = bill_location.getC_BPartner();
 
@@ -30,22 +28,29 @@ public class TaxFactory implements ITaxFactory {
 		if (bp.getC_TaxGroup_ID() == 0)	{
 			throw new TaxCriteriaNotImplemented("This factory can't manage this tax. Delegate to standard Tax factory");
 		}
-				
+
+		// Get all tax definitions.		
 		List<MTaxDefinition> definitions = POFactory.getList(ctx, MTaxDefinition.class, new QParam(MTaxDefinition.COLUMNNAME_C_TaxGroup_ID, bp.getC_TaxGroup_ID()));
 		if (definitions.size() == 1)	{
 			return definitions.get(0).getC_Tax_ID();
 		}
 		
-		MProduct product = MProduct.get(ctx, M_Product_ID);
-		
-		for (MTaxDefinition definition:definitions)	{
-			if (product.getC_TaxCategory_ID() == definition.getC_TaxCategory_ID())	{
-				return definition.getC_Tax_ID();
-			}
+		int C_TaxCategory_ID = 0;
+
+		// Select C_TaxCategory_ID from product or charge
+		if (M_Product_ID != 0)	{
+			MProduct product = MProduct.get(ctx, M_Product_ID);
+			C_TaxCategory_ID = product.getC_TaxCategory_ID();
+		}
+		else {
+			MCharge charge = MCharge.get(ctx, C_Charge_ID);
+			C_TaxCategory_ID = charge.getC_TaxCategory_ID();
 		}
 
+		
+		// Try to find tax definition with the same tax category
 		for (MTaxDefinition definition:definitions)	{
-			if (product.getC_TaxCategory_ID() == definition.getC_TaxCategory_ID())	{
+			if (definition.getC_TaxCategory_ID() == C_TaxCategory_ID)	{
 				return definition.getC_Tax_ID();
 			}
 		}
